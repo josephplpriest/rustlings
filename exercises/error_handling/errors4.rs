a125963eb9fb073224f0d0dab62fd1ff7747eee1
Paul Priest <josephplpriest@gmail.com>
2023-07-16 10:57:15 -0400

    half of rustlings completed

--- exercises/error_handling/errors4.rs
+++ exercises/error_handling/errors4.rs
@@ -1,7 +1,6 @@
 // errors4.rs
 // Execute `rustlings hint errors4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
@@ -14,8 +13,11 @@ enum CreationError {
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
-        // Hmm...? Why is this only returning an Ok value?
-        Ok(PositiveNonzeroInteger(value as u64))
+        match value.cmp(&0) {
+            std::cmp::Ordering::Greater => Ok(PositiveNonzeroInteger(value as u64)),
+            std::cmp::Ordering::Equal => Err(CreationError::Zero),
+            _ => Err(CreationError::Negative),
+        }
     }
 }
 
